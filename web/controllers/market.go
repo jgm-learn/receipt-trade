fd9fb5392f7f73e8b776cea276e21848ab698c90
jgm <jgm@example.com>
2018-12-05 16:53:44 +0800

    新建事务，添加排他锁

--- web/controllers/market.go
+++ web/controllers/market.go
@@ -34,6 +34,7 @@ func (this *MarketController) GetReceiptList() {
 func (this *MarketController) Delist() {
 	var webReply models.WebReply
 	var orderBuy models.OrderBuy
+	//var user models.User
 	var list models.List
 	var userFunds models.UserFunds
 	var userReceipt models.UserReceipt
@@ -46,22 +47,43 @@ func (this *MarketController) Delist() {
 	if err := json.Unmarshal(body, &orderBuy); err != nil {
 		fmt.Println("json Unmarshal 出错：")
 		fmt.Println(err)
-		webReply.Reply = "挂牌失败：json Unmarshal 出错 "
+		webReply.Reply = "摘牌失败：json Unmarshal 出错 "
 		this.Data["json"] = &webReply
 		this.ServeJSON()
 		return
 	}
 	qtyBuy = orderBuy.QtyBuy
 
-	//查询挂单的具体信息
-	err := o.QueryTable("list").Filter("list_id", orderBuy.ListId).One(&list)
-	if err == orm.ErrNoRows {
-		fmt.Println("查询挂单编号为%d的挂单", orderBuy.ListId)
-		webReply.Reply = "摘牌失败：该挂单不存在"
+	//事务处理
+	oTX := orm.NewOrm()
+	errs := oTX.Begin()
+	if errs != nil {
+		fmt.Println("o.Begin ", errs)
+		webReply.Reply = "摘牌失败：o.Begin()出错 "
 		this.Data["json"] = &webReply
 		this.ServeJSON()
 		return
 	}
+
+	err := oTX.Raw("select * from list where list_id = ? for update", orderBuy.ListId).QueryRow(&list)
+	if err != nil {
+		fmt.Println("阻塞中，正在排队", err)
+		webReply.Reply = "摘牌失败：系统太过繁忙，请稍后重试"
+		this.Data["json"] = &webReply
+		this.ServeJSON()
+		return
+	}
+	/*
+		//查询挂单的具体信息
+		err := o.QueryTable("list").Filter("list_id", orderBuy.ListId).One(&list)
+		if err == orm.ErrNoRows {
+			fmt.Println("查询挂单编号为%d的挂单", orderBuy.ListId)
+			webReply.Reply = "摘牌失败：该挂单不存在"
+			this.Data["json"] = &webReply
+			this.ServeJSON()
+			return
+		}
+	*/
 
 	//判断可售仓单数量是否足够
 	if list.QtyRemain < orderBuy.QtyBuy {
@@ -72,7 +94,7 @@ func (this *MarketController) Delist() {
 	}
 	//判断买方是否有足够资金
 	payment := orderBuy.QtyBuy * list.Price //应付款
-	o.QueryTable("user_funds").Filter("user_id", orderBuy.UserId).One(&userFunds)
+	oTX.QueryTable("user_funds").Filter("user_id", orderBuy.UserId).One(&userFunds)
 	if userFunds.AvailableFunds < payment {
 		webReply.Reply = "摘牌失败：资金不足"
 		this.Data["json"] = &webReply
@@ -83,7 +105,7 @@ func (this *MarketController) Delist() {
 	//更新list表
 	list.QtyDeal += qtyBuy
 	list.QtyRemain -= qtyBuy
-	_, err = o.QueryTable("list").Filter("list_id", orderBuy.ListId).Update(orm.Params{
+	_, err = oTX.QueryTable("list").Filter("list_id", orderBuy.ListId).Update(orm.Params{
 		"qty_deal":   list.QtyDeal,
 		"qty_remain": list.QtyRemain,
 	})
@@ -99,7 +121,7 @@ func (this *MarketController) Delist() {
 		userReceipt.QtyFrozen -= qtyBuy
 	*/
 
-	_, err = o.QueryTable("user_receipt").Filter("user_id", list.UserId).Filter("receipt_id", list.ReceiptId).Update(orm.Params{
+	_, err = oTX.QueryTable("user_receipt").Filter("user_id", list.UserId).Filter("receipt_id", list.ReceiptId).Update(orm.Params{
 		//"qty_total":  userReceipt.QtyTotal,
 		//"qty_frozen": userReceipt.QtyFrozen,
 		"qty_total":  orm.ColValue(orm.ColMinus, qtyBuy),
@@ -111,7 +133,7 @@ func (this *MarketController) Delist() {
 	}
 
 	//更新买方user_receipt表
-	err = o.QueryTable("user_receipt").Filter("user_id", orderBuy.UserId).Filter("receipt_id", orderBuy.ReceiptId).One(&userReceipt)
+	err = oTX.QueryTable("user_receipt").Filter("user_id", orderBuy.UserId).Filter("receipt_id", orderBuy.ReceiptId).One(&userReceipt)
 	if err == orm.ErrNoRows {
 		fmt.Println("该用户没有该仓单")
 		userReceipt.UserId = orderBuy.UserId
@@ -120,14 +142,14 @@ func (this *MarketController) Delist() {
 		userReceipt.QtyAvailable = qtyBuy
 		userReceipt.Insert()
 	} else {
-		_, err = o.QueryTable("user_receipt").Filter("user_id", orderBuy.UserId).Filter("receipt_id", orderBuy.ReceiptId).Update(orm.Params{
+		_, err = oTX.QueryTable("user_receipt").Filter("user_id", orderBuy.UserId).Filter("receipt_id", orderBuy.ReceiptId).Update(orm.Params{
 			"qty_total":     orm.ColValue(orm.ColAdd, qtyBuy),
 			"qty_available": orm.ColValue(orm.ColAdd, qtyBuy),
 		})
 	}
 
 	//更新卖方user_funds表
-	_, err = o.QueryTable("user_funds").Filter("user_id", list.UserId).Update(orm.Params{
+	_, err = oTX.QueryTable("user_funds").Filter("user_id", list.UserId).Update(orm.Params{
 		"total_funds":     orm.ColValue(orm.ColAdd, payment),
 		"available_funds": orm.ColValue(orm.ColAdd, payment),
 	})
@@ -137,7 +159,7 @@ func (this *MarketController) Delist() {
 	}
 
 	//更新买方user_funds表
-	_, err = o.QueryTable("user_funds").Filter("user_id", orderBuy.UserId).Update(orm.Params{
+	_, err = oTX.QueryTable("user_funds").Filter("user_id", orderBuy.UserId).Update(orm.Params{
 		"total_funds":     orm.ColValue(orm.ColMinus, payment),
 		"available_funds": orm.ColValue(orm.ColMinus, payment),
 	})
@@ -145,6 +167,9 @@ func (this *MarketController) Delist() {
 		fmt.Printf("更新user_funds表 total_funds,available_funds字段失败 %v\n", err)
 		return
 	}
+
+	//提交事务
+	oTX.Commit()
 
 	webReply.Reply = "摘牌成功"
 	this.Data["json"] = &webReply
